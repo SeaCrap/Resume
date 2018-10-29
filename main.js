395cbdf9067b966a43d2ffc9ac53510b61fc06ed
SeaCrap <506076395@qq.com>
2018-10-29 22:56:13 +0800

    BUG 未解决

--- main.js
+++ main.js
@@ -5,3 +5,16 @@ window.onscroll = (e)=>{
     topNavBar.classList.remove('sticky')
   }
 }
+
+
+let liTags = document.getElementsByClassName('menuTigger')
+console.log(liTags)
+for(let i=0; i<liTags.length; i++){
+  liTags[i].onmouseenter = function(e){
+    console.log(e.currentTarget)
+    let li = e.currentTarget
+    let brother = li.getElementsByTagName('UL')[0]
+    console.log(brother)
+    brother.classList.add('active')
+  }
+}  
