af44ce50bc6bde01df79238ace47844e8297abe2
SeaCrap <506076395@qq.com>
2018-11-07 00:05:26 +0800

    优化：绑定事件和激活分开 && 绑定this

--- js/sticky-topNavBar.js
+++ js/sticky-topNavBar.js
@@ -1,41 +1,32 @@
 //简单版
 !function(){
   var view = document.querySelector('#topNavBar')
-  // 生成 controller 对象
   var controller = {     
-    //有一个 view（为空）
     view: null,
-    //有一个 init 方法
     init: function(view){ 
-      //把 view 存在自己的 view 里边。
-      //现在 view 不为空了 这里的 view是下边controller.init(view)传进来的 
       this.view = view
-      //绑定事件
-      //this.bindEvents() 等价于 this.bindEvents.call(this) 把当前this作为里边的this
       this.bindEvents() 
     },
     bindEvents:function(){
-      //这里的this 需要做两次判断
-      //判断1：这里的 this 在bindEvents 里边的 this.bindEvents.call(this)
-      //判断2：bindEvents 里的 this 是 init 里边的 
-      //this init() 里的 this 是由下边 controller.init.call(controller,view)的 this 也就是 controller
-      //这里的 view 就是上边 view: null 的 view
       var view = this.view       
       window.addEventListener('scroll',(e)=>{
         if(window.scrollY > 0){
-          view.classList.add('sticky')
+         this // 此时的 this 是用户滚动的元素 所以要绑定 this 这样的this才是自己需要改变的
         }else{
           view.classList.remove('sticky')
         }
-      })
+      }.bind(this)) //绑定 this
+    },
+    active: function(){
+      this.view.classList.add('sticky')
+    },
+    deactive: function(){
+    this.view.classList.remove('sticky')
     }
 
   }
    
   controller.init(view)
-  //controller.init.call(controller,view) this 就是 controller
-  //如果一个对象调用一个函数 这个对象就是函数内的 this 
-  //所以这里的view就决定了上边 this.view = view 里的 this 就是 controller
 }.call()
 
 //① 生成 controller 对象
