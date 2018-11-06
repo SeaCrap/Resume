b4ea0735bd69c33f250b889c1986aa5e2a363a5d
SeaCrap <506076395@qq.com>
2018-11-07 00:51:47 +0800

    init-swiper.js优化完毕

--- js/init-swiper.js
+++ js/init-swiper.js
@@ -1,29 +1,31 @@
 !function(){
-  
   var view = document.querySelector('#mySlides')
   var controller = {
+    //controller三个重要属性和一个 init 方法。view/swiper/swiperOptions/init()
     view: null,
     swiper: null, //用到了this.swiper 初始化一下swiper
+    swiperOptions:{ loop: true, 
+      pagination: {
+        el: '.swiper-pagination',
+      },
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+    },
+    
     init: function(view){
       this.view = view //接受到view存起来
-      this.initSwiper() //绑定事件
+      this.initSwiper() //初始化 swiper
     },
     initSwiper: function(){
-      this.wiper = new Swiper (view.querySelector('.swiper-container'), {
-        loop: true, 
-
-        pagination: {
-          el: '.swiper-pagination',
-        },
-
-        navigation: {
-          nextEl: '.swiper-button-next',
-          prevEl: '.swiper-button-prev',
-        },
-      })
+      this.swiper = new Swiper (
+        this.view.querySelector('.swiper-container'), 
+        this.swiperOptions //选项上边已经记下来了
+      ) 
     }
   }
-  
+
   controller.init(view)
 }.call()
 
