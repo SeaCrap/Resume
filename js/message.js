bcb324f6d27ea2e9a9447dc5d24e8e91dd557520
SeaCrap <506076395@qq.com>
2018-11-09 22:36:52 +0800

    获取历史留言并展示在页面

--- js/message.js
+++ js/message.js
@@ -6,6 +6,41 @@ AV.init({
   appKey: APP_KEY
 });
 
+//存数据并把数据显示到ol中
+var query = new AV.Query('Message');//初始化AV对象
+query.find()//查找messages
+  .then(
+    //查找成功把messages放到ol里
+    function (messages) {
+      console.log(messages)
+      console.log(messages[0].attributes)
+      console.log(messages[1].attributes)
+      let arry = messages.map((item)=> item.attributes)
+      arry.forEach((item)=>{
+        let li = document.createElement('li')
+        li.innerText = item.content 
+        //这是避免生成全局变量而这只的局部变量
+        let messgaeList = document.querySelector('#messageList')
+        messageList.appendChild(li)
+      })
+    }, 
+    //查找失败提交此error
+    function (error) {
+      //这里是find函数的异常处理
+    })
+    .then(()=>{},(error)=>{
+      //这里是arry.forEach函数的异常处理
+    })
+
+
+
+
+
+
+
+
+
+//监听submit 提交
 let myForm = document.querySelector('#postMessageFrom')
 myForm.addEventListener('submit', function(e){
   //阻止默认事件 防止刷新页面
@@ -17,8 +52,7 @@ myForm.addEventListener('submit', function(e){
   message.save({
     'content': content
   }).then(function(object) {
-    alert('存入成功')
-    console.log(object)
+    window.location.reload()//帮用户刷新页面
   })
 })
 
@@ -39,7 +73,7 @@ myForm.addEventListener('submit', function(e){
 
 
 /*
-//创建 TestObject 表
+  //创建 TestObject 表
 var X = AV.Object.extend('HuaXie');//TestObject 是 leancloud创建的类名(对应的表名)
 //在表中创建一行数据
 var O = new X();
