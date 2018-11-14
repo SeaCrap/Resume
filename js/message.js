0c660d6af4da53ba4c63d8623efad1d03c05164e
SeaCrap <506076395@qq.com>
2018-11-14 22:37:56 +0800

    message:MVC 之 v-c

--- js/message.js
+++ js/message.js
@@ -1,98 +1,81 @@
-var APP_ID = '4z3xOPBFrVM3sTXGGPQ1DuNJ-gzGzoHsz';
-var APP_KEY = 'u86ebQHzADzbAxBPUOHItGPN';
+!function(){
+  var view = document.querySelector('section.message') 
 
-AV.init({
-  appId: APP_ID,
-  appKey: APP_KEY
-});
+  var controller = {
+    view: null,
+    messageList: null,
+    init: function(view){
+      this.view = view
+      this.messageList = view.querySelector('#messageList')
+      this.form = view.querySelector('form')
+      this.initAV()
+      this.loadMessages()
+      this.bindEvents()
+    },
+    //初始化AV
+    initAV: function(){
+      var APP_ID = '4z3xOPBFrVM3sTXGGPQ1DuNJ-gzGzoHsz';
+      var APP_KEY = 'u86ebQHzADzbAxBPUOHItGPN';
 
-//存数据并把数据显示到ol中
-var query = new AV.Query('Message');//初始化AV对象
-query.find()//查找messages
-  .then(
-    //查找成功把messages放到ol里
-    function (messages) {
-      console.log(messages)
-      console.log(messages[0].attributes)
-      console.log(messages[1].attributes)
-      let arry = messages.map((item)=> item.attributes)
-      arry.forEach((item)=>{
-        let li = document.createElement('li')
-        li.innerText = `${item.name}: ${item.content}`//读 name 和 content
-        //这是避免生成全局变量而这只的局部变量
-        let messgaeList = document.querySelector('#messageList')
-        messageList.appendChild(li)
+      AV.init({ appId: APP_ID, appKey: APP_KEY})
+    },
+    //加载message
+    loadMessages: function(){
+      //存数据并把数据显示到ol中
+      var query = new AV.Query('Message');//初始化AV对象
+      query.find()//查找messages
+        .then(
+          //查找成功把messages放到ol里
+          (messages)=>{ //这里使用箭头函数，要确保this对外不变
+            let arry = messages.map((item)=> item.attributes)
+            arry.forEach((item)=>{
+              let li = document.createElement('li')
+              li.innerText = `${item.name}: ${item.content}`//读 name 和 content
+              //这是避免生成全局变量而这只的局部变量
+              //let messgaeList = document.querySelector('#messageList')
+              this.messageList.appendChild(li)
+            })
+          }
+        )   
+    },
+    
+    //这里除了绑定事件什么也不做 把获取message和提交message分离出去
+    bindEvents: function(){
+      //监听submit 提交
+      this.form.addEventListener('submit', function(e){
+        //阻止默认事件 防止刷新页面
+        e.preventDefault()
+        this.saveMessage()
       })
-    }, 
-    //查找失败提交此error
-    function (error) {
-      //这里是find函数的异常处理
-    })
-    .then(()=>{},(error)=>{
-      //这里是arry.forEach函数的异常处理
-    })
+    },
+    
+    //获取/保存/展示留言
+    saveMessage: function(){
+      //下面好多使用到了Myform 这里可以声明一下
+      let myForm = this.form
 
-
-
-
-
-
-
-
-
-//监听submit 提交
-let myForm = document.querySelector('#postMessageFrom')
-myForm.addEventListener('submit', function(e){
-  //阻止默认事件 防止刷新页面
-  e.preventDefault()
-  //content 是获取 name=content 的 input 的 value
-  let content = myForm.querySelector('input[name=content]').value
-  let name = myForm.querySelector('input[name=name]').value
-  var Message = AV.Object.extend('Message');
-  var message = new Message();
-  message.save({
-    'name': name, //存name
-    'content': content //存 content
-  }).then(function(object) {
-    //window.location.reload()//帮用户刷新页面
-     let li = document.createElement('li')
+      //获取
+      //content 是获取 name=content 的 input 的 value
+      let content = myForm.querySelector('input[name=content]').value
+      let name = myForm.querySelector('input[name=name]').value
+      var Message = AV.Object.extend('Message');
+      var message = new Message();
+      //保存
+      message.save({
+        'name': name, //存name
+        'content': content //存 content
+      }).then(function(object) {
+        //展示
+        let li = document.createElement('li')
         li.innerText = `${object.attributes.name}: ${object.attributes.content}`//读 name 和 content
         //这是避免生成全局变量而这只的局部变量
         let messgaeList = document.querySelector('#messageList')
         messageList.appendChild(li)
         myForm.querySelector('input[name=content]').value = ''//清楚输入内容：用户提交之后 value为空
-  })
-})
+      })
+    }
+  }
+  controller.init(view)
+}.call()
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*
-  //创建 TestObject 表
-var X = AV.Object.extend('HuaXie');//TestObject 是 leancloud创建的类名(对应的表名)
-//在表中创建一行数据
-var O = new X();
-//数据内容是 words: 'Hello world' 保存
-//如果保存成功 运行 alert('LeanCloud Rocks!')
-
-O.save({
-  xxx: 'Hello World!'
-}).then(function(object) {
-  alert('LeanCloud Rocks!');
-}).then(function(){
-  console.log(arguments[0])
-})
-*/
