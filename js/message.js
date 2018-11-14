07a6e52f6e51b4a09e62a23ed662f44292462bf7
SeaCrap <506076395@qq.com>
2018-11-14 23:34:18 +0800

    message:MVC 之 model

--- js/message.js
+++ js/message.js
@@ -1,38 +1,53 @@
 !function(){
   var view = document.querySelector('section.message') 
 
-  var controller = {
-    view: null,
-    messageList: null,
-    init: function(view){
-      this.view = view
-      this.messageList = view.querySelector('#messageList')
-      this.form = view.querySelector('form')
-      this.initAV()
-      this.loadMessages()
-      this.bindEvents()
-    },
-    //初始化AV
-    initAV: function(){
+  var model = {
+   //初始化AV
+    init: function(){
       var APP_ID = '4z3xOPBFrVM3sTXGGPQ1DuNJ-gzGzoHsz';
       var APP_KEY = 'u86ebQHzADzbAxBPUOHItGPN';
 
       AV.init({ appId: APP_ID, appKey: APP_KEY})
     },
+
+    fetch: function(){//获取数据
+      var query = new AV.Query('Message');//初始化AV对象
+      return query.find()//返回 Promise 对象
+    },
+    
+    save: function(){// 存储数据      
+      var Message = AV.Object.extend('Message');
+      var message = new Message();
+      //保存
+     return message.save({//返回 promise 对象
+        'name': name, 
+        'content': content 
+      }) 
+    },
+  }
+  var controller = {
+    view: null,
+    messageList: null,
+    model: null,
+    init: function(view, model){
+      this.view = view
+      this.model = model
+            
+      this.messageList = view.querySelector('#messageList')
+      this.form = view.querySelector('form')
+      this.model.init()
+      this.loadMessages()
+      this.bindEvents()
+    },
+   
     //加载message
     loadMessages: function(){
-      //存数据并把数据显示到ol中
-      var query = new AV.Query('Message');//初始化AV对象
-      query.find()//查找messages
-        .then(
-          //查找成功把messages放到ol里
+      this.model.fetch().then(
           (messages)=>{ //这里使用箭头函数，要确保this对外不变
             let arry = messages.map((item)=> item.attributes)
             arry.forEach((item)=>{
               let li = document.createElement('li')
-              li.innerText = `${item.name}: ${item.content}`//读 name 和 content
-              //这是避免生成全局变量而这只的局部变量
-              //let messgaeList = document.querySelector('#messageList')
+              li.innerText = `${item.name}: ${item.content}`
               this.messageList.appendChild(li)
             })
           }
@@ -53,18 +68,11 @@
     saveMessage: function(){
       //下面好多使用到了Myform 这里可以声明一下
       let myForm = this.form
-
       //获取
       //content 是获取 name=content 的 input 的 value
       let content = myForm.querySelector('input[name=content]').value
       let name = myForm.querySelector('input[name=name]').value
-      var Message = AV.Object.extend('Message');
-      var message = new Message();
-      //保存
-      message.save({
-        'name': name, //存name
-        'content': content //存 content
-      }).then(function(object) {
+      this. moedl.save(name, content).then(function(object) {
         //展示
         let li = document.createElement('li')
         li.innerText = `${object.attributes.name}: ${object.attributes.content}`//读 name 和 content
@@ -75,7 +83,7 @@
       })
     }
   }
-  controller.init(view)
+  controller.init(view, model)
 }.call()
 
 
